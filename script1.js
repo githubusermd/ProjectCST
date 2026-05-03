74b6362d6f988c8f0bab8098afabdd63d7bb0bf4
Md Mosharof Hossen <mosharofm540@gmail.com>
2026-05-04 01:19:52 +0600

    addation

--- script1.js
+++ script1.js
@@ -1,22 +1,25 @@
-<!-- Firebase SDK (MODERN VERSION) -->
 <script type="module">
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
   import { 
     getFirestore, collection, addDoc, getDocs, deleteDoc, doc, updateDoc 
   } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
   const firebaseConfig = {
-    apiKey: "YOUR_KEY",
-    authDomain: "YOUR_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_BUCKET",
-    messagingSenderId: "YOUR_MSG_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyAyEhyeZKSmMoJSgRWb-rtO1ARZ2o46-i8",
+    authDomain: "project-cst.firebaseapp.com",
+    projectId: "project-cst",
+    storageBucket: "project-cst.firebasestorage.app",
+    messagingSenderId: "16968914162",
+    appId: "1:16968914162:web:d90b32e3aed455f6c66c6b",
+    measurementId: "G-Q5PPVNXS84"
   };
 
   const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
   const db = getFirestore(app);
 
+  // 🔥 make it available globally
   window.db = db;
   window.firebaseFns = { collection, addDoc, getDocs, deleteDoc, doc, updateDoc };
 </script>
