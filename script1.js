73bab49d218b61f7cbd7e04b2f3b73488129d4de
Md Mosharof Hossen <mosharofm540@gmail.com>
2026-05-06 23:50:15 +0600

    ff

--- script1.js
+++ script1.js
@@ -1,30 +1,30 @@
-<script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-  import {
-    getFirestore,
-    collection,
-    addDoc,
-    getDocs,
-    deleteDoc,
-    doc,
-    updateDoc
-  } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyAyEhyeZKSmMoJSgRWb-rtO1ARZ2o46-i8",
-    authDomain: "project-cst.firebaseapp.com",
-    projectId: "project-cst",
-    storageBucket: "project-cst.appspot.com",
-    messagingSenderId: "16968914162",
-    appId: "1:16968914162:web:d90b32e3aed455f6c66c6b"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyAyEhyeZKSmMoJSgRWb-rtO1ARZ2o46-i8",
+  authDomain: "project-cst.firebaseapp.com",
+  projectId: "project-cst",
+  storageBucket: "project-cst.appspot.com",
+  messagingSenderId: "16968914162",
+  appId: "1:16968914162:web:d90b32e3aed455f6c66c6b"
+};
 
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-  window.db = db;
-  window.firebaseFns = { collection, addDoc, getDocs, deleteDoc, doc, updateDoc };
+window.db = db;
+window.firebaseFns = { collection, addDoc, getDocs, deleteDoc, doc, updateDoc };
 
-  // IMPORTANT
-  window.firebaseReady = true;
-</script>
+// ✅ VERY IMPORTANT
+window.firebaseReady = true;
+
+console.log("Firebase Loaded ✅");
